b66b275031bb4c5452c9a639ff5290ae88cb2c79
khashashin <khashashin@gmail.com>
2017-12-31 20:59:48 +0100

    configured static file settings

--- crmapp/settings.py
+++ crmapp/settings.py
@@ -112,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
-    {
+    {ш
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
@@ -136,3 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
