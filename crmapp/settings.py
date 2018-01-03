8ece78e3825572c60c4026637c45fb115be22e1b
khashashin <khashashin@gmail.com>
2018-01-03 01:37:14 +0100

    "created the subscriber form"

--- crmapp/settings.py
+++ crmapp/settings.py
@@ -55,7 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'marketing',
-    'ultimatedjango.crmapp.subscribers',
+    'subscribers',
 ]
 
 MIDDLEWARE = [
