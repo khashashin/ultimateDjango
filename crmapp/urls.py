adc54754c49e1bb59af22232cc24d58130d22918
khashashin <khashashin@gmail.com>
2018-01-04 23:52:12 +0100

    "create login & logout URLs"

--- crmapp/urls.py
+++ crmapp/urls.py
@@ -30,6 +30,12 @@ urlpatterns = [
 
 
     # Login/Logout URLs
+    (r'^login/$',
+    'django.contrib.auth.views.login', {'template_name': 'login.html'}
+    ),
+    (r'^logout/$',
+        'django.contrib.auth.views.logout', {'next_page': '/login/'}
+    ),
 
 
     # Account related URLs
