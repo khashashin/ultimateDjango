6d0f7ec9ecc8a967c450b2a7c832e544ba59247c
khashashin <khashashin@gmail.com>
2018-01-04 13:28:12 +0100

    fixes

--- crmapp/settings.py
+++ crmapp/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 from django.core.exceptions import ImproperlyConfigured
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -158,9 +157,11 @@ STATICFILES_DIRS = (
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-django_heroku.settings(locals())
-
 # Parse database configuration from $DATABASE_URL
 if ENV_ROLE == 'production':
     import dj_database_url
     DATABASES['default'] =  dj_database_url.config()
+
+# Configure Django App for Heroku
+import django_heroku
+django_heroku.settings(locals())
