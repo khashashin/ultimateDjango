57c439039956656265e145bea6cea771f0d78424
khashashin <khashashin@gmail.com>
2018-01-06 16:46:58 +0100

    "modified new account view to allow for edits"

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path, re_path
-from .views import account_detail,account_cru
+from .views import account_detail, account_cru
 
 urlpatterns = [
     re_path(r'^$', account_detail, name='account_detail'),
-    re_path(r'^$', account_cru, name='account_update'),
+    re_path(r'^edit/$', account_cru, name='account_update'),
 ]
