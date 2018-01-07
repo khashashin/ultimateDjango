9c411ff2be879065ba302f8fc8d5f56dea49f7bc
khashashin <khashashin@gmail.com>
2018-01-07 21:49:24 +0100

    "created the communications model"

--- communications/models.py
+++ communications/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from accounts.models import Account
+
+from shortuuidfield import ShortUUIDField
 
 # Create your models here.
+
+class Communication(models.Model):
+    uuid = ShortUUIDField(unique=True)
+    TYPE_LIST = (
+        (1, 'Meeting'),
+        (2, 'Phone'),
+        (3, 'Email'),
+    )
+    subject = models.CharField(max_length=50)
+    notes = models.TextField()
+    kind = models.PositiveSmallIntegerField(choices=TYPE_LIST)
+    date = models.DateField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    created_on = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'communications'
+
+    def __unicode__(self):
+        return u"%s" % self.subject
+
+    @models.permalink
+    def get_absolute_url(self):
+        return 'comm_detail', [self.uuid]
+
+    @models.permalink
+    def get_update_url(self):
+        return 'comm_update', [self.uuid]
+
+    @models.permalink
+    def get_delete_url(self):
+        return 'comm_delete', [self.id]
