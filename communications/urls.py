67f6d043338b3cc1a7b8186f870e8b27dfb69483
khashashin <khashashin@gmail.com>
2018-01-08 05:12:16 +0100

    "created the edit communication URL"

--- communications/urls.py
+++ communications/urls.py
@@ -1,7 +1,8 @@
 from django.urls import re_path
-from .views import comm_detail
+from .views import comm_detail, comm_cru
 
 
 urlpatterns = [
     re_path(r'^$', comm_detail, name="comm_detail"),
+    re_path(r'^edit/$', comm_cru, name='comm_update'),
 ]
